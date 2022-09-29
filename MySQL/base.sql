d2cbce6625f1726e4cfaeb3c859fc964e69b135e
estx07 <87713240+estx07@users.noreply.github.com>
2022-09-29 15:50:23 -0300

    Iniciando script base.

---
+++ MySQL/base.sql
@@ -0,0 +1,17 @@
+#Script de MySQL Workbench para generar Base de datos del TP.
+
+
+#Crear Database:
+
+CREATE DATABASE disqueria
+DEFAULT CHARACTER SET 'utf8mb4'
+COLLATE 'utf8mb4_0900_ai_ci';
+
+
+#Seleccionamos Database:
+
+use disqueria;
+
+
+
+#Crear Tablas
