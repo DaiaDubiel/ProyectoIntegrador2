c2071437a6548768c1e6385720d5a90ae596cae1
carlosbml85 <84574849+carlosbml85@users.noreply.github.com>
2022-09-30 13:33:41 -0300

    Update base.sql

--- MySQL/base.sql
+++ MySQL/base.sql
@@ -20,7 +20,7 @@ CREATE TABLE `disqueria`.`album` (
   `codigo_album` VARCHAR(45) NOT NULL,
   `nombre` VARCHAR(50) NOT NULL,
   `cant_temas` INT NOT NULL,
-  `fecha_lanzamiento` DATE NOT NULL,
+  `fecha_lanzamiento` YEAR(4) NOT NULL,
   `precio` decimal(65,2) NOT NULL,
   `cantidad` INT NOT NULL,
   `caratula` VARCHAR(200) NULL,
@@ -155,7 +155,6 @@ ADD CONSTRAINT `fk_id_discografica`
 
 
 #Insert a las tablas
-
 insert into discografica values('1','Sony Music');
 insert into discografica values('2','Island Records');
 insert into discografica values('3','ISO RECORDS');
@@ -177,8 +176,32 @@ insert into discografica values('18','COLUMBIA');
 insert into discografica values('19','Capitol Records');
 insert into discografica values('20','Discos CNR Chile');
 
-insert into album values('1','2','602448064851','Holy Fvck','16','2022-01-10','2560.55','98','https://i.discogs.com/sROffi3nsEcQd3K7LLFCWM9NjS6TXpNY5BpA6jFjdVs/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTI0MjQ3/MjIzLTE2NjA5Mzgy/NjItNzIyOC5qcGVn.jpeg');
-insert into album values('2','1','194397420524','MAS CARO QUE AYER','18','2021-06-18','300.48','16','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlSVH7Moy8fgjMpumDJWpnL7iMnBr44entXRZ-ip96V39kwFN2bMXJq-YVz7M-iZXDAP0&usqp=CAU');
+insert into album values('1','2','602448064851','Holy Fvck','16','2022','2560.55','98','https://i.discogs.com/sROffi3nsEcQd3K7LLFCWM9NjS6TXpNY5BpA6jFjdVs/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTI0MjQ3/MjIzLTE2NjA5Mzgy/NjItNzIyOC5qcGVn.jpeg');
+insert into album values('2','1','194397420524','MAS CARO QUE AYER','18','2021','300.48','16','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlSVH7Moy8fgjMpumDJWpnL7iMnBr44entXRZ-ip96V39kwFN2bMXJq-YVz7M-iZXDAP0&usqp=CAU');
+insert into album values('3','3','508222 0','HEATHEN','12','2002','25.36','25','https://i.discogs.com/zPO_rrlppom0sAP_rgOnxCJznJqqw3-YW4NjqTYwtKI/rs:fit/g:sm/q:90/h:599/w:599/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTU5NjUx/MS0xMjI5NjM3NDkx/LmpwZWc.jpeg');
+insert into album values('4','4','0034553235','BLACK TIE WHITE NOISE','4','2003','142.37','47','https://i.discogs.com/2VdYF1zlYBIkgYAQu0P-e0GaoHryN6rRfqa6pQzw-UE/rs:fit/g:sm/q:90/h:599/w:594/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTEwNjY2/NjQtMTE4OTUzOTUw/MS5qcGVn.jpeg');
+insert into album values('5','5','B0027184_02','04:44','13','2017','1500','59','https://i.discogs.com/mY5jx7tLrGpIQvJKyfD8L0-PulDev7PgEXBajZ1HwCQ/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTEwNTM3/MTY1LTE1Njg2Njk4/NTQtMzAwMS5qcGVn.jpeg');
+insert into album values('6','6','B0004813_02','LATE REGISTRATION','21','2005','123','1','https://i.discogs.com/VaGAJ7bFhiP1JY5nrQDFGGw2mmbU7OH0xm_j9msSaNM/rs:fit/g:sm/q:90/h:592/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTQ5NDE2/OS0xNTAxMjY5NDM1/LTMxNzIuanBlZw.jpeg');
+insert into album values('7','7','B0001533-02','Resurrection','14','2003','14.78','3','https://i.discogs.com/pWMx5ErnYekeFomn1R8X4SjOFRBVm6csm5LUtcLuvLk/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTI1MTY5/MS0xMzk3ODgxMDU4/LTY3NzQuanBlZw.jpeg');
+insert into album values('8','1','88697567792','Fuerza Natural','14','2009','151','345','https://i.discogs.com/Ng_TF7utB2rOpjVQDxssTwlERny9eKYBZS2XFFRpu7U/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTI2Mjkx/OTgtMTQyODI1OTM5/Ni05MzIyLmpwZWc.jpeg');
+insert into album values('9','8','190296666988','MUSIC OF THE SPHERES','12','2021','169','49','');
+insert into album values('10','2','0005797700','SONGS OF EXPERIENCE','17','2017','500','0','');
+insert into album values('11','10','7A0F8109','AND JUSTICE FOR ALL','9','1988','320','0','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTl3PUIPfPPG2ZhxOK66BqaisoFd2-L-dYCXqMMax-xwYoRmtTGPT1BAyg3hhKHyAWYG8E&usqp=CAU');
+insert into album values('12','11','R2671074','101','20','2021','316.59','57','https://i.discogs.com/ac4UDphQS0Z834SOOdufDWjxQceEN2hEVvVfylHBeWE/rs:fit/g:sm/q:90/h:592/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTIxMjI0/MjkwLTE2Mzg2MzA3/MTAtODQ4NS5qcGVn.jpeg');
+insert into album values('13','1','19439885311','DOPAMINA','13','2021','516','12','https://i.discogs.com/A3eSclNDe4ciuX6W7DQJJ-5H6lyoKVzqFlXcn9JZPR0/rs:fit/g:sm/q:90/h:520/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTE4NzUx/MzQyLTE2MjExNjY4/NTItNzI4Mi5qcGVn.jpeg');
+insert into album values('14','1','190759900925','ADN','16','2019','22','13','https://i.discogs.com/G2JgS66JLJT4a5isr-h14FbOUPfupHOLaa88jmNLOK8/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTE0Njkw/NDE0LTE1Nzk3MDkx/MjUtMjkyNi5qcGVn.jpeg');
+insert into album values('15','1','88697567784-2','THE LAST','18','2009','34','46','https://i.discogs.com/JyMBDcKmCftrPK78lNB4cxW0mWuCfbX2hZLbOSk-lTI/rs:fit/g:sm/q:90/h:540/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTEwMDc1/ODkwLTE0OTExOTA5/MTgtMjk3Mi5qcGVn.jpeg');
+insert into album values('16','1','MLMC 0554','GODS PROJECT','15','2005','28','48','https://i.discogs.com/3VNdvMaIP7R_IYeMxvQ-CTOoURdsKrsbKKQlSgY04Y8/rs:fit/g:sm/q:90/h:569/w:568/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTM3NjM5/NTEtMTM0MzQ1NzM4/Ny03OTY2LmpwZWc.jpeg');
+insert into album values('17','12','K-136','BACHATA ROSA','10','1990','34','58','https://i.discogs.com/j-kpoILafNGHjoryjd2sOAs4iXOelNwUSoxLd1_hobU/rs:fit/g:sm/q:90/h:600/w:598/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTM2Nzgy/MjgtMTM0MDEwNzI5/OS0zMTc0LmpwZWc.jpeg');
+insert into album values('18','13','7421-80227-2','ABRAZAME MUY FUERTE','12','2000','34','69','https://i.discogs.com/7XfzRQbUGcFurhFWVLDpYC7Mxay-UqyT5b-abYrK7nc/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTEwNDAw/NDExLTE1MjQzNTUz/ODgtMzgyMS5qcGVn.jpeg');
+insert into album values('19','14','0000050102','CORAZON DE PAPEL','12','2000','67','22','https://i.discogs.com/v5PCgaS2Ga_5lCsum_mEuuIfKxTnOwaQf4sSxdwBaDU/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTExNDc5/MjQ0LTE1MTcwNjg2/MjAtNjQxMC5qcGVn.jpeg');
+insert into album values('20','15','056 078','Phil Collins','10','1984','98','1','https://i.discogs.com/tE2BtkaUnn2n3HKMxJ-FQYF8PsHYXsJxsREWyS_OErM/rs:fit/g:sm/q:90/h:600/w:599/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTY3Mzc4/MS0xNTU5MjMwMzc4/LTk1ODAuanBlZw.jpeg');
+insert into album values('21','16','814 287-1','Genesis','9','1983','247','3','https://i.discogs.com/53qsKzEyTkh_KxRmXgWy8mAxDfATHtSxasN1KPYFPXo/rs:fit/g:sm/q:90/h:593/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTE2MDU0/MzQtMTY2MjY3NDMw/OC02OTczLmpwZWc.jpeg');
+insert into album values('22','4','0000047266','Thriller','9','1982','1320','11','https://i.discogs.com/t3Gzrzp1_DT27bSfVynl5bCmqWqddfUT5ohPHuUDtyE/rs:fit/g:sm/q:90/h:600/w:597/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTE2NTk1/MjctMTY2NDIwNDcy/Mi05NDU4LmpwZWc.jpeg');
+insert into album values('23','18','COL 481367 1','Daydream','12','1995','12','2','https://i.discogs.com/GdNcjvx_s5QytpgbThAyx8jEIW8saXBTaky5Z5joiio/rs:fit/g:sm/q:90/h:592/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTE1NDQ0/MzEtMTQzNTcwMzg0/OS0xMDAyLmpwZWc.jpeg');
+insert into album values('24','19','5 099907 142710','Charm School','12','2011','987','0','https://i.discogs.com/P-Ty0E8Y5dXZBmk14deaOwyt58eiQ4vPZ3UnsJlFy0w/rs:fit/g:sm/q:90/h:350/w:350/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTI3MjI2/MDUtMTI5ODExOTgy/Ny5qcGVn.jpeg');
+insert into album values('25','20','0000017720','Live In Sydney 1991','10','2016','624','1','https://i.discogs.com/abWajKVPDOV28DxzUDwxCCUYqCXoKQMR1tvvBQIZQzU/rs:fit/g:sm/q:90/h:452/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTkxNjI4/NjEtMTQ3NTg5MTc1/OS0yNzIxLmpwZWc.jpeg');
+
 
 insert into genero values('1','Latino');
 insert into genero values('2','Pop');
@@ -293,4 +316,28 @@ insert into album_tema values('2','15');
 insert into album_tema values('2','16');
 insert into album_tema values('2','17');
 insert into album_tema values('2','18');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
